58edc7cd6c97a35653efd7e80bcb212505799ebe
raul140298 <raul.jeri@pucp.edu.pe>
2023-11-22 12:22:14 -0500

    Create new turn system

--- scenes/gameplay/CharacterBody2D.gd
+++ scenes/gameplay/CharacterBody2D.gd
@@ -5,11 +5,10 @@ var actionsPerTurn = 2
 var health = 100
 var guard = 0
 var actionsList =[0,1,2,3,4]
-var actions = ["ShootSlow", "ShootFast", "Movement", "Health", "Guard"]
+var actions = ["ShootFast", "ShootSlow", "Movement", "Health", "Guard"]
 var steps = [1,1,2,1,1]
 var actionsAvailable = []
 var stepsAvailable = []
-var selected = [0,0,0,0,0]
  
 
 func _ready():
@@ -18,80 +17,94 @@ func _ready():
 
 
 func _process(delta):
-	#if actionsAvailable.has(0) && stepsAvailable[0] > 0:
+	if Input.is_action_just_pressed("FinishTurn"):
+		await get_tree().create_timer(2).timeout
+		print()
+		print("NEXT TURN==============================")
+		StartTurn()
+	
+	if actionsPerTurn > 0:
+		if Input.is_action_just_pressed("Action1"):
+			ActionChosen(0)
 		
-	#if actionsAvailable.has(1) && stepsAvailable[1] > 0:
+		if Input.is_action_just_pressed("Action2"):
+			ActionChosen(1)
+			
+		if Input.is_action_just_pressed("Action3"):
+			ActionChosen(2)
 		
-	if actionsAvailable.has(2) && stepsAvailable[2] > 0:
+	if stepsAvailable[2] > 0:
 		if !isMoving:
-			if Input.is_action_pressed("ui_right"):
+			if Input.is_action_just_pressed("ui_right"):
 				if self.position.x < -64:
 					MovePlayer(Vector2i.RIGHT)
-			elif Input.is_action_pressed("ui_left"):
+			elif Input.is_action_just_pressed("ui_left"):
 				if self.position.x > -448:
 					MovePlayer(Vector2i.LEFT)
-			elif Input.is_action_pressed("ui_down"):
+			elif Input.is_action_just_pressed("ui_down"):
 				if self.position.y < 256:
 					MovePlayer(Vector2i.DOWN)
-			elif Input.is_action_pressed("ui_up"):
+			elif Input.is_action_just_pressed("ui_up"):
 				if self.position.y > -256:
 					MovePlayer(Vector2i.UP)
 	
-	if actionsAvailable.has(3) && stepsAvailable[3] > 0:
-		if Input.is_action_pressed("ui_select"):
-			StartActionPerTurn(3)
-			health += 25
-			if health > 100:
-				health = 100
+	
+func ActionChosen(id):
+	actionsPerTurn -= 1
+	print("    You chosed ", actions[actionsAvailable[id]])
+	print("=======================================")
+	stepsAvailable[actionsAvailable[id]] += steps[actionsAvailable[id]]
+	
+	InstantAction(actionsAvailable[id])
+	
+	if actionsPerTurn > 0:
+		RandomizeActions()
 		
-	#if actionsAvailable.has(4) && stepsAvailable[4] > 0:
+func InstantAction(id):
+	if id == 0 && stepsAvailable[0] > 0:
+		print("Shoot fast Action")
+		print("=======================================")	
+		StartActionPerTurn(0)
+		
+	if id == 1 && stepsAvailable[1] > 0:
+		print("Shoot slow Action")
+		print("=======================================")	
+		StartActionPerTurn(1)
 
-func StartTurn():
-	actionsPerTurn = 2
+	if id == 3 && stepsAvailable[3] > 0:
+		print("Health Action")
+		print("=======================================")	
+		StartActionPerTurn(3)
+		health += 25
+		if health > 100:
+			health = 100
+		
+	if id == 4 && stepsAvailable[4] > 0:
+		print("Guard Action")
+		print("=======================================")	
+		StartActionPerTurn(4)
+		
+	
+func RandomizeActions():
 	var aux = actionsList
 	aux.shuffle()
 	actionsAvailable = aux.slice(0,3) #Arreglo de 3 ids desordenados
-	stepsAvailable = [0,0,0,0,0]
-	selected = [0,0,0,0,0]
-	
-	for i in range(3):
-		stepsAvailable[actionsAvailable[i]] = steps[actionsAvailable[i]]
-		print(actions[actionsAvailable[i]], " ", stepsAvailable[actionsAvailable[i]])
-	
+	print(actions[actionsAvailable[0]], " ", actions[actionsAvailable[1]], " ", actions[actionsAvailable[2]])
 	print("=======================================")
+
+
+func StartTurn():
+	actionsPerTurn = 2
+	stepsAvailable = [0,0,0,0,0]
+	RandomizeActions()
 	
 func StartActionPerTurn(id):
-	if stepsAvailable[id] == 0:
-		return
-	
-	if stepsAvailable[id] == steps[id]:
-		actionsPerTurn -= 1
-		selected[id] = 1
-		
-		if actionsPerTurn == 0:
-			actionsAvailable = []
-			for i in range(5):
-				if selected[i] == 1:
-					actionsAvailable.append(i)
-				else:
-					stepsAvailable[i] = 0
-				
 	if stepsAvailable[id] > 0:
 		stepsAvailable[id] -= 1
-		if stepsAvailable[id] == 0:
-			actionsAvailable.erase(id)
 	
-	print("ActionsLeft: ", actionsPerTurn)
 	print(actions[id], " ", stepsAvailable[id])
 	print("=======================================")
 	
-	var aux = 0
-	for i in range(5):
-		aux += stepsAvailable[i]
-		
-	if aux == 0:
-		await get_tree().create_timer(1).timeout
-		StartTurn()
  
 func MovePlayer(direction):
 	StartActionPerTurn(2)
